7fe1ac7d57fa4040d6716c104f5386b721fdd39c
Harsha Siva Akash Saladi <harshasivaakashsaladi@gmail.com>
2026-04-27 05:04:47 +0530

    Fix: add explicit extensions to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +14,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
+    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
   },
 });
